7e05489467c5e957915b9ba6ae12e98890524f4e
Kristof <you@example.com>
2021-11-16 23:03:33 +0100

    2. feladat kész

--- src/hu/petrik/Exercises/Feladat2.java
+++ src/hu/petrik/Exercises/Feladat2.java
@@ -1,7 +1,35 @@
 package hu.petrik.Exercises;
 
+import hu.petrik.Multihalmaz.Multihalmaz;
+
 public class Feladat2 {
     public static void run(){
+        Multihalmaz<String> termelo = new Multihalmaz<>();
+        Multihalmaz<String> kereskedo = new Multihalmaz<>();
+        for(int i = 0; i < 500; i++){
+            if(i < 30){
+                termelo.add("golden");
+            }
+            if(i < 50){
+                kereskedo.add("golden");
+            }
+            if(i < 100){
+                termelo.add("jonagold");
+                kereskedo.add("starking");
+            }
+            termelo.add("idared");
+        }
 
+        System.out.println("Termelő:");
+        System.out.println(termelo);
+
+        System.out.println("Kereskedő:");
+        System.out.println(kereskedo);
+
+        System.out.println("Termelőnél marad:");
+        System.out.println(termelo.different(kereskedo));
+
+        System.out.println("Kereskedő vesz:");
+        System.out.println(termelo.intersection(kereskedo));
     }
 }
--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -1,25 +1,11 @@
 package hu.petrik;
 
+import hu.petrik.Exercises.Feladat2;
 import hu.petrik.Multihalmaz.Multihalmaz;
 
 public class Main {
 
     public static void main(String[] args) {
-        Multihalmaz<String> a = new Multihalmaz<>();
-        Multihalmaz<String> b = new Multihalmaz<>();
-        for(int i = 0; i < 10; i++){
-            a.add("alma");
-            a.add("alma");
-            b.add("alma");
-            a.add("körte");
-            b.add("körte");
-            b.add("körte");
-        }
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(a.different(b));
-        System.out.println(b.different(a));
-        System.out.println(a);
-        System.out.println(b);
+        Feladat2.run();
     }
 }
