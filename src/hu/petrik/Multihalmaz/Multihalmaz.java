93bf9d7505739b122ddea6843c5c0c79d30fb229
Kristof <you@example.com>
2021-11-16 22:24:56 +0100

    Műveletek kész

--- src/hu/petrik/Multihalmaz/Multihalmaz.java
+++ src/hu/petrik/Multihalmaz/Multihalmaz.java
@@ -56,9 +56,17 @@ public class Multihalmaz<T>{
     public boolean isEqual(Multihalmaz<T> masikHalmaz){
         boolean equals = true;
         if(elemLista.size() == masikHalmaz.getSize()){
-            /*for (int i = 0; i < elemLista.size(); i++){
-
-            }*/
+            for (int i = 0; i < elemLista.size(); i++){
+                if(masikHalmaz.isElement(elemLista.get(i))){
+                    int index = masikHalmaz.elemLista.indexOf(elemLista.get(i));
+                    if(mennyisegLista.get(i) != masikHalmaz.mennyisegLista.get(index)){
+                        equals = false;
+                    }
+                }
+                else{
+                    equals = false;
+                }
+            }
         }
         else{
             equals = false;
@@ -66,11 +74,69 @@ public class Multihalmaz<T>{
         return equals;
     }
 
-    /*
-    remove
-    unio
-    intersection
-    isEqual
-    different
-     */
+    public boolean isPartOf(Multihalmaz<T> masikHalmaz){
+        boolean partOf = true;
+        for(int i = 0; i < elemLista.size(); i++){
+            if (!masikHalmaz.isElement(elemLista.get(i))){
+                partOf = false;
+            }
+        }
+        return partOf;
+    }
+
+    public Multihalmaz<T> unio(Multihalmaz<T> masikHalmaz){
+        Multihalmaz<T> unio = masikHalmaz;
+        for(int i = 0; i < elemLista.size(); i++){
+            T elem = elemLista.get(i);
+            if(unio.isElement(elem)){
+                int index = unio.elemLista.indexOf(elem);
+                unio.mennyisegLista.set(index, mennyisegLista.get(i));
+            }
+            else{
+                unio.elemLista.add(elem);
+                unio.mennyisegLista.add(mennyisegLista.get(i));
+            }
+        }
+        return unio;
+    }
+
+    public Multihalmaz<T> intersection(Multihalmaz<T> masikHalmaz){
+        Multihalmaz<T> intersection = new Multihalmaz<>();
+        for (int i = 0; i < elemLista.size(); i++){
+            T elem = elemLista.get(i);
+            if(masikHalmaz.isElement(elem)){
+                intersection.elemLista.add(elem);
+                intersection.mennyisegLista.add(Math.min(mennyisegLista.get(i), masikHalmaz.mennyisegLista.get(masikHalmaz.elemLista.indexOf(elem))));
+            }
+        }
+        return intersection;
+    }
+
+    public Multihalmaz<T> different(Multihalmaz<T> masikHalmaz){
+        Multihalmaz<T> different = this;
+        for (int i = 0; i < different.getSize(); i++){
+            T elem = elemLista.get(i);
+            if(masikHalmaz.isElement(elem)){
+                int mennyiseg = mennyisegLista.get(i) - masikHalmaz.mennyisegLista.get(masikHalmaz.elemLista.indexOf(elem));
+                if(mennyiseg <= 0){
+                    different.elemLista.remove(i);
+                    different.mennyisegLista.remove(i);
+                    i--;
+                }
+                else{
+                    different.mennyisegLista.set(i, mennyiseg);
+                }
+            }
+        }
+        return different;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        for(int i = 0; i < elemLista.size(); i++){
+            s += elemLista.get(i) + " " + mennyisegLista.get(i) + "\n";
+        }
+        return s;
+    }
 }
